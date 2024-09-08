a34d603b768ea3944dfa1afe1c6a0db3b8e11cf9
kendelline <joannekenisa17@gmail.com>
2024-09-08 17:53:43 +0700

    First Push to PWS

--- main/views.py
+++ main/views.py
@@ -3,7 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 def show_main(request):
     context = {
-        'name' : 'joanne',
+        'name' : 'Joanne',
+        'class' : 'PBP C'
     }
 
     return render(request, "main.html", context)
